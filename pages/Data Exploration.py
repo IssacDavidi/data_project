34ea1e7e2f14171e5b997b31e2f7de9d8eceb50b
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-01 19:19:52 +0200

    Add files via upload

---
+++ pages/Data Exploration.py
@@ -0,0 +1,40 @@
+# Seassion State
+import streamlit as st
+import pandas as pd
+import streamlit_pandas as sp
+from pandasql import sqldf
+
+
+st.title('Explore the data on your own!')
+@st.cache_data
+def load_data():
+    df = pd.read_csv('https://raw.githubusercontent.com/IssacDavidi/data_project/main/steimatzky_cleaned.csv')
+    return df
+
+df = load_data()
+
+
+custom_widget_look = {
+						'Category': 'multiselect',
+						'Sub Category': 'multiselect'
+
+
+}
+
+ignore_columns = ['description']
+
+all_widgets = sp.create_widgets(df, custom_widget_look, ignore_columns=ignore_columns)
+res = sp.filter_df(df, all_widgets)
+st.write(res)
+
+
+
+# Create an adventure button and style it
+
+m = st.markdown("""
+<style>
+div.stButton > button:first-child {
+    background-color: rgb(42, 139, 168);
+}
+</style>""", unsafe_allow_html=True)
+
