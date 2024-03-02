fb34dc749b93df7056231373f3d37752ef1c8b10
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 19:16:03 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -42,7 +42,8 @@ if st.button('Toggle Data Table'):
 # Show or hide data table based on button state
 if st.session_state.show_data_table:
     st.dataframe(df)
-    with st.expander(st.image('https://github.com/IssacDavidi/data_project/blob/main/photos/cat2.jpg?raw=true')):
+    with st.expander('About the data table'):
+        st.image('https://github.com/IssacDavidi/data_project/blob/main/photos/cat2.jpg?raw=true', width=30')
         st.write('The information obtained from the Steimatzky website underwent a web scraping procedure. This dataset comprises 978 books, each available in both physical and digital formats. Any missing values were filled with averages.')
 
 
