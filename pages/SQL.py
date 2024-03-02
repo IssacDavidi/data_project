952788c79491f5348c97e9774cff93282196267a
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 15:25:18 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from sqlalchemy import create_engine
 import pandas as pd
+import os
 
 st.write(st.secrets.db_credentials.username)
+st.write("DB username:", st.secrets["db_username"])
