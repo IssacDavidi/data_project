4ef14c1c4e53fb1fcca0d817b3804111998abc81
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 12:30:37 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -1,18 +1,1 @@
 # This is sql test stuff
-
-import streamlit as st
-import pandas as pd
-import sqlalchemy
-from st_supabase_connection import SupabaseConnection
-
-@st.cache_data
-def sql_query(txt):
-    conn = st.connection("supabase",type=SupabaseConnection)
-    df = conn.query(txt)
-    return df
-
-
-query = st.text_area('Explore the data by running sql queries', 'SELECT * FROM steimatzky;')
-if st.button('Run Query'):
-    result = sql_query(query)
-    st.dataframe(result)
