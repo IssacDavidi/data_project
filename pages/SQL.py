75acf2d8e0e2d98af3129ea32c47ed0f4e57e534
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 13:24:50 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -3,14 +3,12 @@ from st_supabase_connection import SupabaseConnection
 import pandas as pd
 import sqlalchemy
 
-@st.cache_data
-def sql_query(txt):
-    conn = st.connection('supabase', type= SupabaseConnection)
-    df = conn.query(txt)
-    return df
+# Initialize connection.
+conn = st.connection("supabase",type=SupabaseConnection)
 
+# Perform query.
+rows = conn.query("*", table="books", ttl="10m").execute()
 
-query = st.text_area('Explore the data by running sql queries', 'SELECT * FROM books;')
-if st.button('Run Query'):
-    result = sql_query(query)
-    st.dataframe(result)
+# Print results.
+for row in rows.data:
+    st.write(f"{row['name']} has a :{row['pet']}:")
