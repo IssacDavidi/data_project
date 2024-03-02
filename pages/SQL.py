6c36f677179f98022b1cf28241bf777cc9e0fabd
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 14:21:38 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -9,5 +9,5 @@ conn = st.connection("supabase",type=SupabaseConnection)
 # SQL query
 rows = conn.query("*", table="books", ttl="10m").execute()
 
-st.write(type(rows))
-st.write(type(rows.data))
+for row in rows.data:
+  st.write(row)
