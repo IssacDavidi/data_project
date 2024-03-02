8edd2411e25b66231c70acf71e5021e9c431f36a
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 15:49:19 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -4,18 +4,9 @@ import pandas as pd
 import os
 
 # Retrieve Supabase URL and API key from Streamlit secrets
-supabase_url = st.secrets["db_username"]
-supabase_key = st.secrets["db_password"]
+user = st.secrets["db_username"]
+pwd = st.secrets["db_password"]
 
 # Create a SQLAlchemy engine
-engine = create_engine(f"postgresql://{supabase_key}@{supabase_url}/postgres")
+engine = create_engine('postgresql://user:pwd@pga.h.filess.io:5432/books_pocketbarn')
 
-
-# Custom SQL query
-custom_query = "SELECT * FROM books"
-
-# Execute the query and get the result as a Pandas DataFrame
-df = pd.read_sql_query(custom_query, con=engine)
-
-# Display the DataFrame
-st.write(df)
