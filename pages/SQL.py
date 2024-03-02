475e1d80bc6b88b938e1bcd21d1e9c0f253dbab2
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 13:22:20 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -1,9 +1,16 @@
 import streamlit as st
 from st_supabase_connection import SupabaseConnection
+import pandas as pd
+import sqlalchemy
 
-# Initialize connection.
-conn = st.connection("supabase",type=SupabaseConnection)
+@st.cache_data
+def sql_query(txt):
+    conn = st.connection('books', type= SupabaseConnection)
+    df = conn.query(txt)
+    return df
 
-# Perform query.
-rows = conn.query("*", table="books", ttl="10m").execute()
-st.write(rows)
+
+query = st.text_area('Explore the data by running sql queries', 'SELECT * FROM books;')
+if st.button('Run Query'):
+    result = sql_query(query)
+    st.dataframe(result)
