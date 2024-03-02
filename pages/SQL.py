d4702231bffa0a4b9e52e7f73a82a92f42b5cd6e
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 13:47:43 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -1,14 +1,15 @@
 import streamlit as st
 from st_supabase_connection import SupabaseConnection
 import pandas as pd
-import sqlalchemy
 
 
-
-def sql_query(txt):
+def sql_query(txt, table_name='books'):
     # Use SupabaseConnection and provide the URL and API key
     conn = st.connection("supabase", type=SupabaseConnection)
-    df = conn.query(txt, table='books')
+    
+    # Assuming SyncSelectRequestBuilder has an execute method to get a DataFrame
+    df = conn.query(txt, table=table_name).execute()
+    
     return df
 
 query = st.text_area('Explore the data by running SQL queries', 'SELECT * FROM books;')
