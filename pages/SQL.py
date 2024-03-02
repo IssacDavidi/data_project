36bdbe7abf1c8de19de2fd1d8d28b430c6333ab7
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 14:15:30 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -9,18 +9,7 @@ st_supabase_client = StSupabaseClient()
 # Initialize connection.
 conn = st.connection("supabase",type=SupabaseConnection)
 
-# Your SQL query
-query = "SELECT * FROM books"
+# SQL query
+rows = conn.query("*", table="books", ttl="10m").execute()
 
-# Execute the query and get the result
-result = st_supabase_client.query("*", table="books", ttl=0).execute()
-
-# Check if data is present in the result
-if result.data:
-    # Convert the data to a Pandas DataFrame
-    df = result.data
-
-    # Display the DataFrame in Streamlit
-    st.dataframe(df)
-else:
-    st.warning("No data found for the given query.")
+st.write(rows)
