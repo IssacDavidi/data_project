1dedbd2384c23625ff763e6fb197945a55294c05
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 13:22:59 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -5,7 +5,7 @@ import sqlalchemy
 
 @st.cache_data
 def sql_query(txt):
-    conn = st.connection('books', type= SupabaseConnection)
+    conn = st.connection('supabase', type= SupabaseConnection)
     df = conn.query(txt)
     return df
 
