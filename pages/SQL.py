6f76e6a7d70c911f711b061ce0c537afdd1dd9e4
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 13:17:52 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -1,1 +1,9 @@
-# This is sql test stuff
+import streamlit as st
+from st_supabase_connection import SupabaseConnection
+
+# Initialize connection.
+conn = st.connection("supabase",type=SupabaseConnection)
+
+# Perform query.
+rows = conn.query("*", table="books", ttl="10m").execute()
+st.write(rows)
