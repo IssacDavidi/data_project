163b71b2084d1586d35b27b9d0e03b8a5c8252aa
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 14:08:29 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -2,6 +2,8 @@ import streamlit as st
 from st_supabase_connection import SupabaseConnection
 import pandas as pd
 
+# Initialize Supabase client
+st_supabase_client = StSupabaseClient()
 
 # Initialize connection.
 conn = st.connection("supabase",type=SupabaseConnection)
