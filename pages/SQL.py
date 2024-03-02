e58ed29da072e5645b567f65c0de7d0386907d82
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 13:39:09 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -3,9 +3,16 @@ from st_supabase_connection import SupabaseConnection
 import pandas as pd
 import sqlalchemy
 
+# Retrieve Supabase URL from Streamlit secrets
+supabase_url = st.secrets["SUPABASE_URL"]
+
+# Retrieve Supabase API key from Streamlit secrets
+supabase_key = st.secrets["SUPABASE_KEY"]
+
 @st.cache_data
 def sql_query(txt):
-    conn = SupabaseConnection("superbase",type=SupabaseConnection, url = 'https://pmswmtbodrexbljaimym.supabase.co')  # Use SupabaseConnection to create a connection
+    # Use SupabaseConnection and provide the URL and API key
+    conn = SupabaseConnection(url=supabase_url, key=supabase_key)
     df = conn.query(txt)
     return df
 
