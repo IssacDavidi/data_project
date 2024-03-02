e6d5993c5a58c72bb05a00511e316bb25e3bc897
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 16:21:48 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -2,12 +2,7 @@ import streamlit as st
 from sqlalchemy import create_engine
 import pandas as pd
 import os
-import psycopg2
 
-# Retrieve Supabase URL and API key from Streamlit secrets
-user = st.secrets["db_username"]
-pwd = st.secrets["db_password"]
 
-# Create a SQLAlchemy engine
-engine = create_engine('postgresql://user:pwd@pga.h.filess.io:5432/books_pocketbarn')
+st.write('Comming soon')
 
