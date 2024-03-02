19442f86382662f7e41534ae52844f18a41e7a67
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 12:03:50 +0200

    Add files via upload

---
+++ pages/SQL.py
@@ -0,0 +1,17 @@
+# This is sql test stuff
+
+import streamlit as st
+import pandas as pd
+import sqlalchemy
+
+@st.cache_data
+def sql_query(txt):
+    conn = st.connection('project_books', type= SupabaseConnection)
+    df = conn.query(txt)
+    return df
+
+
+query = st.text_area('Explore the data by running sql queries', 'SELECT * FROM steimatzky;')
+if st.button('Run Query'):
+    result = sql_query(query)
+    st.dataframe(result)
