ba9a8b55f6dee1d242dc6eda17195d189fe19ced
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 19:03:33 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -12,7 +12,6 @@ with col1:
   st.subheader('Lorem ipsum dolor sit amet')
   st.write('consectetur adipiscing elit. Praesent tristique quis sem ut tempus. Curabitur in dui in nibh posuere fringilla.')
   st.write('Nulla facilisi. Ut quis consectetur leo. Cras mollis sapien nec dolor ultricies elementum. Cras tempor ipsum vel ex ullamcorper, eget feugiat neque lobortis. Fusce nec lectus risus.')
-  st.write('Praesent imperdiet turpis et risus consequat fermentum. Donec sed sollicitudin arcu. Pellentesque in neque vel justo auctor fringilla aliquet ut sapien. Fusce mauris dolor, tristique vel nulla nec, ultricies tincidunt nibh')
 with col2:
   st.image('https://github.com/IssacDavidi/data_project/blob/main/photos/cat1.png?raw=true', 'A beautiful cat', width=600)
   
