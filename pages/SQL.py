9d1a73782b20123485bee31bd9d004bbb1c2ec21
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 13:51:11 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -1,21 +1,11 @@
 import streamlit as st
 from st_supabase_connection import SupabaseConnection
-import pandas as pd
 
-@st.cache_data
-def sql_query(txt, table_name='books'):
-    # Use SupabaseConnection and provide the URL and API key
-    conn = st.connection("supabase", type=SupabaseConnection)
-    
-    # Specify the columns explicitly in the query
-    query = f'SELECT column1, column2, ... FROM {table_name};'
-    
-    # Assuming SyncSelectRequestBuilder has an execute method to get a DataFrame
-    df = conn.query(query).execute()
-    
-    return df
+# Initialize connection.
+conn = st.connection("supabase",type=SupabaseConnection)
 
-query = st.text_area('Explore the data by running SQL queries', 'SELECT column1, column2, ... FROM books;')
-if st.button('Run Query'):
-    result = sql_query(query)
-    st.dataframe(result)
+# Perform query.
+rows = conn.query("*", table="books", ttl="10m").execute()
+
+for row in rows:
+    st.write(row)
