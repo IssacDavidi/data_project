0c5fedb4d19f7006ce41f3bfe156d3510b671e08
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-02 18:57:31 +0200

    Update SQL.py

--- pages/SQL.py
+++ pages/SQL.py
@@ -9,7 +9,7 @@ import os
 col1, col2 = st.columns(2)
 
 with col1:
-  st.header('test')
+  st.subheader('Hello there, welcome to my data analysis project! I hope you like it')
 with col2:
   st.image('https://github.com/IssacDavidi/data_project/blob/main/photos/cat1.png?raw=true', 'A beautiful cat', width=600)
   
