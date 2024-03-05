76a13ed447e44baf65a0903ed0afb1048e7cee16
IssacDavidi <110817275+IssacDavidi@users.noreply.github.com>
2024-03-05 17:27:50 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -77,6 +77,6 @@ if st.session_state.show_data_table:
         except:
             st.error(':x: An error occoured running the query provided')
     if st.button("Next Page"):
-        st.switch_page("SQL.py")
+        switch_page('SQL')
 
 # REWRITE THE CODE WITH SESSION STATE FOR EACH BUTTON
